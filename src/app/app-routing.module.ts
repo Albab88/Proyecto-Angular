0761ede011317ce889c1c9a1204354f972f14827
Alba <albabarragan88@gmail.com>
2025-06-26 18:42:03 -0300

    Se hace el ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisqueriaAboutComponent } from './disqueria-about/disqueria-about.component';
+import { DisqueriaMusicComponent } from './disqueria-music/disqueria-music.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path: '',
+    redirectTo: '/music',
+    pathMatch: 'full'
+  },
+  {
+    path: 'music',
+    component: DisqueriaMusicComponent
+  },
+  {
+    path: 'about',
+    component: DisqueriaAboutComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
